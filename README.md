1c30f9f32f797887cd7ea9bd4bd203e7d6e12559
tecnosue <susanaparacuellos@gmail.com>
2022-04-22 18:54:57 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Tecnosue JS Footer
+
+> This should be used in the following manner
+
+Install with the command:
+
+```
+npm install --save tecnosue-js-footer
+```
+
+Add to a JavaScript Project with the following code:
+
+```js
+import { footer } from 'tecnosue-js-footer';
+
+footer('Some Name');
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var moment = require('moment');
+
+/**
+* Returns a string element with a footer and updating year
+* @param {string} name
+* @return {string}
+*/
+
+exports.footer = function (name) {
+    return "Copyright " + moment().format('YYYY') + " " + name + " All rights reserved";
+};
